8c671d861d5b48636d9ded76d026c26731c68b57
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-01 15:26:12 +0000

    statica data

---
+++ src/lib/data.ts
@@ -0,0 +1,29 @@
+export const navLinks=[
+    {name:'github', href:'/'},
+    {name:'linkedIn', href:'/for_artist'},
+    {name:'x', href:'/for_artist'},
+    {name:"lets'talk", href:'/for_buyers'}
+]
+
+export const skills=[
+    {name:'Nextjs'},
+    {name:'React'},
+    {name:'Python'},
+    {name:'Tailwind'},
+    {name:'Django'},
+    {name:'JavaScript'},
+    {name:'Threejs'},
+    {name:'Frontend'},
+    {name:'machine-learning'},
+    {name:'Web3'},
+    {name:'Typescript'},
+    {name:'Docker'},
+    {name:'Flutter'},
+    {name:'fullstack'},
+    {name:'rest-apis'},
+    {name:'ai'},
+    {name:'ui/ux'},
+
+
+
+]
