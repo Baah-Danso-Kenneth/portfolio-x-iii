a36866a915cc2d32a2e1f2b9a13adaa7e73d5d46
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-01 15:26:49 +0000

    Structure codebase to align with specific alignment

---
+++ src/components/sections/homePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SkillsHolder from './skills/skillsHolder'
+
+function HomePage() {
+  return (
+    <div>
+       <SkillsHolder/>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/sections/homePage/skills/skillsHolder.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import SkillsContent from './skillsContent'
+import { skills } from '@/lib/data'
+
+function SkillsHolder() {
+  return (
+    <div className='my-10 border py-5 border-l-0 border-r-0'>
+    <div className='flex overflow-hidden group '>
+        <div className='animate-loop-scroll group-hover:paused'>
+            <div className='flex'>
+                {skills.map(({name},index)=>(
+                <SkillsContent name={name} key={index}/>
+                 ))}
+            </div>
+        </div>
+
+        <div className='animate-loop-scroll group-hover:paused'>
+            <div className='flex'>
+                {skills.map(({name},index)=>(
+                <SkillsContent name={name} key={index}/>
+                 ))}
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default SkillsHolder
