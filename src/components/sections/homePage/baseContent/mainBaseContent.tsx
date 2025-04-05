4c94efd8f3e91af8ea0916592c17dc3c97a89c72
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-05 20:03:59 +0000

    sticker image

--- src/components/sections/homePage/baseContent/mainBaseContent.tsx
+++ src/components/sections/homePage/baseContent/mainBaseContent.tsx
@@ -1,12 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 function MainBaseContent() {
   return (
-    <div className='mx-10 border p-10 border-t-0 border-b-0'>
+    <div className='mx-10 border p-10 border-t-0 border-b-0 relative'>
         <div><h1 className='font-dmMono text-4xl'>Hi i&apos;m kenneth Danso</h1></div>
 
-        <div className='py-10'>
-            <h1 className='text-[8rem] font-bowlby leading-[8rem]'>i build  amazing stuff using code</h1>
+        <div className='py-10 z-50'>
+            <h1 className='text-[8rem] font-bowlby leading-[8rem] z-50'>i build  magical stuff using code</h1>
+        </div>
+
+        <div className='absolute top-0 right-0 transform rotate-45  '>
+          <Image src="/images/sticker.png" alt="" className="z-20" width={450} height={250}/>
         </div>
 
     </div>
